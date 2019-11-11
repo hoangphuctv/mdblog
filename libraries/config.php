4c03e2bb23275324ec3679be6b8669ca25ebceb6
PhucTH <phucth@568e.vn>
2019-11-11 11:01:16 +0700

    add default config struct

--- libraries/config.php
+++ libraries/config.php
@@ -1,5 +1,13 @@
 <?php
 
 class Config extends Sample{
-	
+	public $site_name     = "Lập trình";
+	public $author        = "Phuc Tran Hoang";
+	public $post_per_page = 10;
+	public $post_dir      = ROOT."/posts";
+	public $permalink     = "/blog/{YEAR}/{MONTH}/{DAY}/{SLUG}/";
+	public $theme         = 'default';
+	public $ga_id         = '';
+	public $fb_app_id     = '';
+	public $debug         = true;
 }
