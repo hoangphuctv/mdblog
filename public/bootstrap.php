a9c4ca61e2a7307205932715ab7adaa0a760e86b
PhucTH <phucth@568e.vn>
2019-11-11 11:01:42 +0700

    Update bootstrap.php fix case character file name

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -5,8 +5,8 @@ define('CACHE', ROOT . '/cached');
 define('LIBRARIES', ROOT . '/libraries');
 
 
-require_once LIBRARIES . '/Sample.php';
-require_once LIBRARIES . '/Config.php';
+require_once LIBRARIES . '/sample.php';
+require_once LIBRARIES . '/config.php';
 
 unset($file);
 unset($files);
@@ -16,6 +16,9 @@ $config = new Config(json_decode(file_get_contents(ROOT.'/config.json')));
 
 define('POST',  $config->get('post_dir', '/posts'));
 
+if (!is_dir(POST)) {
+	echo "Post dir: ", POST, " not found";exit;
+}
 if (isset($config->debug) && $config->debug) {
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
