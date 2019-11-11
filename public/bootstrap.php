aa847ccec929cfe8416edae68bc17f17f140cb15
PhucTH <phucth@568e.vn>
2019-11-11 17:31:26 +0700

    Show msg error if config invalid format

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -7,12 +7,20 @@ define('LIBRARIES', ROOT . '/libraries');
 
 require_once LIBRARIES . '/sample.php';
 require_once LIBRARIES . '/config.php';
+require_once LIBRARIES . '/post.php';
 
 unset($file);
 unset($files);
 
 global $config;
-$config = new Config(json_decode(file_get_contents(ROOT.'/config.json')));
+$cf = json_decode(file_get_contents(ROOT.'/config.json'), true);
+if ($cf === null) {
+	echo "Invalid config format<br/>";
+	readfile(ROOT.'/config.json');
+	exit;
+}
+$config = new Config($cf);
+unset($cf);
 
 define('POST',  $config->get('post_dir', '/posts'));
 
