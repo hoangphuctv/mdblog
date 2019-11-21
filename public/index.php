9b603e7f1ed0e20b72d3976ff4b592f8c4695237
PhucTH <phucth@568e.vn>
2019-11-21 10:58:01 +0700

    route page-num

--- public/index.php
+++ public/index.php
@@ -7,7 +7,10 @@ $uri = str_replace('.html', '.md', $uri);
 $md_origin = $uri;
 $mdfile = POST. '/'. $uri;
 
-if ($uri == "/") {
+if ($uri == "/" || preg_match("/\/page-(\d+)/", $uri, $match_page)) {
+	if (!empty($match_page[1])) {
+		$_GET['page'] = (int)$match_page[1];
+	}
 	include CTRL . '/home.php';
 }else {
 	$post = find_post($uri);
