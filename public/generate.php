4b68475757809277571decc71c7ef9da3fa5b188
PhucTH <phucth@568e.vn>
2019-11-22 17:24:07 +0700

    generate pages and index html

--- public/generate.php
+++ public/generate.php
@@ -2,13 +2,16 @@
 include_once __DIR__ . '/bootstrap.php';
 
 // init server variables
-
 $_SERVER['HTTP_HOST'] = parse_url($config->base_url, PHP_URL_HOST);
 $_SERVER['SERVER_PORT'] = parse_url($config->base_url, PHP_URL_PORT);
+
+define('BASE_URL', $config->base_url);
 
 $all_cache = CACHE."/all";
 $lines = file_get_contents($all_cache);
 $lines = explode("\n", $lines);
+
+// generate post detail
 foreach ($lines as $md_origin) {
 	$_SERVER['REQUEST_URI'] = $md_origin;
 	if (empty($md_origin)) { continue; }
@@ -17,11 +20,31 @@ foreach ($lines as $md_origin) {
 
 	ob_start();
 	include CTRL . '/single.php';
-	$content = ob_get_contents();
+	$html = ob_get_contents();
 	ob_clean();
+	write_post_file($post, $html);
+}
+
+// generate index and pages
+$page_number = ceil(count($lines) / $config->post_per_page);
+for ($page = 1; $page < $page_number; $page++) {
+	$_GET['page'] = $page;
+	ob_start();
+	include CTRL . '/home.php';
+	$html = ob_get_contents();
+	ob_clean();
+	if ($page == 1) {
+		// write home page
+		write_page_file(0, $html);
+	}
+	write_page_file($page, $html);
+}
+println("Done");
+
+function write_post_file($post, $html) {
 	$htmlfile = $post->link;
 	if ($post->draft == 'true') {
-		continue;
+		return false;
 	}
 	if (!preg_match('/\/(.+)\.(\w+)$/', $htmlfile, $m)) {
 		$htmlfile .= 'index.html';
@@ -35,6 +58,23 @@ foreach ($lines as $md_origin) {
 		mkdir($dirname, 0775, true);
 	}
 	println('Writing file', $htmlfile);
-	file_put_contents($dirname . '/' . $basename, $content);
+	file_put_contents($dirname . '/' . $basename, $html);
+	return true;
 }
-println("Done");
+
+function write_page_file($page, $html) {
+	if ($page == 0) {
+		$htmlfile = "/index.html";
+	}else {
+		$htmlfile = "/page-{$page}/index.html";
+	}
+	$fullpath =  STATICPATH . $htmlfile;
+	$dirname = STATICPATH . dirname($htmlfile);
+	$basename = basename($htmlfile);
+
+	if (!is_dir($dirname)) {
+		mkdir($dirname, 0775, true);
+	}
+	println('Writing file', $htmlfile);
+	file_put_contents($dirname . '/' . $basename, $html);
+}
