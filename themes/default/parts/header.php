604da5afb1d6f4868500e8bc0d5433c4fbea6e45
PhucTH <tv.hoangphuc@gmail.com>
2020-08-20 15:36:14 +0700

    fix form search submit to google

--- themes/default/parts/header.php
+++ themes/default/parts/header.php
@@ -1,17 +1,18 @@
 <div class="container">
 	<h2><a class="navbar-brand mr-auto mr-lg-0" href="/"><?php echo $config->site_name ?></a></h2>
 
-	<form class="form-inline my-2 my-lg-0" id="frmsearch" action="/">
-	    <input class="form-control mr-sm-2" name="q" type="text" placeholder="Search" aria-label="Search">
+	<script>
+	  function search_submit(){
+		  var q = document.body.querySelector('#text-q');
+		  console.log(q )
+		  q.value = q.value + " site:" + location.hostname;
+		  return true;
+	  };
+	</script>
+	<form class="form-inline my-2 my-lg-0" id="frmsearch" action="https://google.com/search" onsubmit="search_submit(this)">
+		<input class="form-control mr-sm-2" name="q" id="text-q" type="text" placeholder="Search on google" aria-label="Search">
 	    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 	  </form>
-	<script>
-		$(function(){
-			$('#frmsearch').submit(function(){
-				location.href='https://google.com/search?q='+ document.all('q').value + " sitename:" + location.hostname; return false;
-			});
-		})
-	</script>
 
 	<p class="separator"></p>
 </div>
